f3323e0a489dead2867aff1e37b47c98bae9d3ba
Christian Nelson <christian@carbonfive.com>
2014-01-22 10:56:58 -0800

    Raygun-zapped skeleton.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MobileVideoUpload::Application.config.session_store :cookie_store, key: '_mobile_video_upload_session'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+MobileVideoUpload::Application.routes.draw do
+
+  root to: 'pages#root'
+
+end
